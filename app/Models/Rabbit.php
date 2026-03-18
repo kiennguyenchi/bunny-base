3736e83d9dfa7dfa47e2151420e1ffbe6857c5fe
Kien Nguyen <kiennguyenchi11@gmail.com>
2026-03-18 14:18:10 -0400

    implement relationship to dad/mom

--- app/Models/Rabbit.php
+++ app/Models/Rabbit.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Rabbit extends Model
 {
@@ -18,4 +19,14 @@ class Rabbit extends Model
         'sire_id',
         'dam_id',
     ];
+
+    public function sire(): BelongsTo
+    {
+        return $this->belongsTo(Rabbit::class, 'sire_id');
+    }
+
+    public function dam(): BelongsTo
+    {
+        return $this->belongsTo(Rabbit::class, 'dam_id');
+    }
 }
